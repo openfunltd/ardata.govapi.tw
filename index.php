d90f494d101a681f972c51fff8305d54c78c1de9
typingmonk <tmonk.hsieh@tmonk.tw>
2025-05-19 12:56:45 +0800

    Route setting to api resource and swagger related page

--- index.php
+++ index.php
@@ -5,6 +5,26 @@ MiniEngine::dispatch(function($uri){
     if ($uri == '/robots.txt') {
         return ['index', 'robots'];
     }
+
+    if ($uri == '/swagger') {
+        return ['swagger', 'ui'];
+    }
+
+    if ($uri == '/swagger.yaml') {
+        return ['swagger', 'index'];
+    }
+
+    // 假設資料名稱為 Example
+    // 處理 /examples, /example/{id} => example
+    // 依照有沒有 libraries/API/Type/{Example}.php 決定
+    //   /examples => ApiController::collectionAction({"type":"example"})
+    //   /example/{id} => ApiController::itemAction({"type":"example","id":["id"]})
+    //   /example/{id}/foo => ApiController::itemAction({"type":"example","id":["id"]})
+    $param = LYAPI_Helper::getApiType($uri);
+    if ($param) {
+        return $param;
+    }
+
     // default
     return null;
 });
