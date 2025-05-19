12eb4414f8acd3c663b7af7227380b106e650318
typingmonk <tmonk.hsieh@tmonk.tw>
2025-05-19 12:25:59 +0800

    Add config fields for access elasticserach db

--- config.sample.inc.php
+++ config.sample.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-putenv('APP_NAME=Mini Engine sample application');
-putenv('DATABASE_URL=pgsql://user:password@localhost:5432/dbname');
-putenv('SESSION_SECRET=___SECRET___');
-putenv('SESSION_DOMAIN='); // optional
+putenv('ELASTIC_URL=');
+putenv('ELASTIC_PREFIX=');
+putenv('ELASTIC_USER=');
+putenv('ELASTIC_PASSWORD=');
